03af802e4e5836233994f8454e3fb07e68595eec
Gustavo Mello <gugamello2014@yahoo.com>
2024-03-14 21:41:33 -0300

    test: :test_tube: (WIP) add tests for useTodo hook

--- src/domain/todo/presenters/todo_presenter/useTodo.test.ts
+++ src/domain/todo/presenters/todo_presenter/useTodo.test.ts
@@ -1,6 +1,10 @@
-// sum.test.js
-import { expect, test } from 'vitest';
+import { test } from 'vitest';
+import { makeFakeTodoVo } from '../../../../__test__/makeFakeTodoVo';
+import { spySaveTodo } from '../../application/TodoService';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+describe('useTodo', () => {
+  test('should be able to remove a todo', () => {
+    spySaveTodo.mockResolvedValueOnce(makeFakeTodoVo());
+  });
+  test('should update todo list after creating a new one', () => {});
 });
