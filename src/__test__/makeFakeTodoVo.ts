799cec7e7855a66ae4c95ef983a9dfad9cc38add
Gustavo Mello <gugamello2014@yahoo.com>
2024-03-14 21:40:58 -0300

    test: :clown_face: add fake todo vo mock

---
+++ src/__test__/makeFakeTodoVo.ts
@@ -0,0 +1,16 @@
+import { TodoPriority } from '../domain/todo/infra/entities/todo_entity';
+import { TodoVo } from '../domain/todo/infra/entities/todo_vo';
+
+export function makeFakeTodoVo(properties?: Partial<TodoVo>): TodoVo {
+  return {
+    id: '1',
+    message: 'Todo 1',
+    priority: TodoPriority.LOW,
+    completed: false,
+    completedAt: null,
+    createdAt: Date.now(),
+    updatedAt: null,
+    deletedAt: null,
+    ...properties,
+  };
+}
