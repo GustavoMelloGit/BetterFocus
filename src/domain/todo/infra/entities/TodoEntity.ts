a775a30ffc93c434008129c2523bbd77f5179bbf
Gustavo Mello <gugamello2014@yahoo.com>
2024-03-15 22:12:22 -0300

    refactor(TodoEntity): :recycle: change entity into a encapsulation of properties

--- src/domain/todo/infra/entities/TodoEntity.ts
+++ src/domain/todo/infra/entities/TodoEntity.ts
@@ -19,19 +19,20 @@ export interface ITodoEntity {
   createdAt: TodoDate;
   updatedAt: TodoDate | null;
   deletedAt: TodoDate | null;
-  isCompleted(): boolean;
+
   isDeleted(): boolean;
+  delete(): void;
 }
 
 export class TodoEntity implements ITodoEntity {
-  id: TodoId;
-  message: string;
-  completed: boolean;
-  priority: TodoPriority;
-  completedAt: TodoDate | null;
-  createdAt: TodoDate;
-  updatedAt: TodoDate | null;
-  deletedAt: TodoDate | null;
+  private _id: TodoId;
+  private _message: string;
+  private _completed: boolean;
+  private _priority: TodoPriority;
+  private _completedAt: TodoDate | null;
+  private _createdAt: TodoDate;
+  private _updatedAt: TodoDate | null;
+  private _deletedAt: TodoDate | null;
 
   constructor(
     id: TodoId,
@@ -43,26 +44,73 @@ export class TodoEntity implements ITodoEntity {
     updatedAt: TodoDate | null = null,
     deletedAt: TodoDate | null = null
   ) {
-    this.id = id;
-    this.message = message;
-    this.completed = completed;
-    this.priority = priority;
-    this.completedAt = completedAt;
-    this.createdAt = createdAt;
-    this.updatedAt = updatedAt;
-    this.deletedAt = deletedAt;
+    this._id = id;
+    this._message = message;
+    this._completed = completed;
+    this._priority = priority;
+    this._completedAt = completedAt;
+    this._createdAt = createdAt;
+    this._updatedAt = updatedAt;
+    this._deletedAt = deletedAt;
+  }
+  set message(message: string) {
+    this._message = message;
+    this._updatedAt = Date.now();
   }
 
-  isCompleted(): boolean {
-    return this.completed;
+  get message(): string {
+    return this._message;
+  }
+
+  set priority(priority: TodoPriority) {
+    this._priority = priority;
+    this._updatedAt = Date.now();
+  }
+
+  get priority(): TodoPriority {
+    return this._priority;
+  }
+
+  set completed(completed: boolean) {
+    this._completed = completed;
+    this._completedAt = completed ? Date.now() : null;
+    this._updatedAt = Date.now();
+  }
+
+  get completed(): boolean {
+    return this._completed;
+  }
+
+  get id(): TodoId {
+    return this._id;
+  }
+
+  get completedAt(): TodoDate | null {
+    return this._completedAt;
+  }
+
+  get createdAt(): TodoDate {
+    return this._createdAt;
+  }
+
+  get updatedAt(): TodoDate | null {
+    return this._updatedAt;
+  }
+
+  get deletedAt(): TodoDate | null {
+    return this._deletedAt;
   }
 
   isDeleted(): boolean {
-    return this.deletedAt !== null;
+    return !!this._deletedAt;
+  }
+
+  delete(): void {
+    this._deletedAt = Date.now();
   }
 }
 
-export function createTodoEntity(todo: TodoVo): ITodoEntity {
+export function createTodoEntity(todo: TodoVo) {
   return new TodoEntity(
     todo.id,
     todo.message,
@@ -79,8 +127,8 @@ export function todoEntityToVo(todo: ITodoEntity): TodoVo {
   return {
     id: todo.id,
     message: todo.message,
-    completed: todo.completed,
     priority: todo.priority,
+    completed: todo.completed,
     completedAt: todo.completedAt,
     createdAt: todo.createdAt,
     updatedAt: todo.updatedAt,
