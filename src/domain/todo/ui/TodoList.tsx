3a767e7709ded9674005aac52578b23dd01c26f3
Gustavo Mello <gugamello2014@yahoo.com>
2024-03-15 22:15:07 -0300

    feat: :sparkles: add complete todo checkbox

--- src/domain/todo/ui/TodoList.tsx
+++ src/domain/todo/ui/TodoList.tsx
@@ -1,13 +1,18 @@
 import { useTodo } from '../presenters/todo_presenter/useTodo';
 
 export default function TodoList(): JSX.Element {
-  const { addTodo, removeTodo, allTodos } = useTodo();
+  const { addTodo, removeTodo, allTodos, completeTodo } = useTodo();
   return (
     <div>
       <button onClick={() => addTodo('hello')}>Add Todo</button>
       <ul>
         {allTodos.map((todo) => (
           <li key={todo.id}>
+            <input
+              type='checkbox'
+              checked={todo.completed}
+              onChange={() => completeTodo(todo.id)}
+            />
             {todo.message}
             <button onClick={() => removeTodo(todo.id)}>Remove</button>
           </li>
