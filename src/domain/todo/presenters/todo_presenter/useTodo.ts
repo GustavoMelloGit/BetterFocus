b6b3bf9cf087ec711e697576699ec97ebbf4614f
Gustavo Mello <gugamello2014@yahoo.com>
2024-03-15 21:16:12 -0300

    feat: :sparkles: add useTodo functions

--- src/domain/todo/presenters/todo_presenter/useTodo.ts
+++ src/domain/todo/presenters/todo_presenter/useTodo.ts
@@ -1,37 +1,64 @@
 import { useEffect, useState } from 'react';
 import { makeTodoService } from '../../application/TodoService';
-import { TodoId, TodoPriority } from '../../infra/entities/todo_entity';
-import { TodoVo } from '../../infra/entities/todo_vo';
+import { TodoId, TodoPriority } from '../../infra/entities/TodoEntity';
+import { TodoVo } from '../../infra/entities/TodoVo';
 
 const service = makeTodoService();
 
 export function useTodo(): UseTodo {
   const [allTodos, setAllTodos] = useState<TodoVo[]>([]);
 
-  function removeTodo(id: TodoId) {
-    service.removeTodo(id);
+  async function removeTodo(todoId: TodoId) {
+    try {
+      await service.removeTodo(todoId);
+      setAllTodos((prev) => prev.filter((todo) => todo.id !== todoId));
+    } catch (e) {
+      console.error(e);
+    }
   }
 
-  function addTodo(message: string) {
-    service.saveTodo({
-      message,
-      priority: TodoPriority.LOW,
-    });
+  async function addTodo(message: string) {
+    try {
+      const savedTodo = await service.saveTodo({
+        message,
+        priority: TodoPriority.MEDIUM,
+      });
+      setAllTodos((prev) => [...prev, savedTodo]);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function completeTodo(todoId: string) {
+    try {
+      const completedTodo = await service.completeTodo(todoId);
+      setAllTodos((prev) => {
+        const index = prev.findIndex((todo) => todo.id === todoId);
+        prev[index] = completedTodo;
+        return prev;
+      });
+    } catch (e) {
+      console.error(e);
+    }
   }
 
   useEffect(() => {
-    service.getAllTodos().then(setAllTodos);
+    service.getAllTodos().then((todos): void => {
+      setAllTodos(todos);
+    });
   }, []);
 
   return {
     removeTodo,
     addTodo,
+    completeTodo,
     allTodos,
   };
 }
 
 type UseTodo = {
-  removeTodo: (id: TodoId) => void;
-  addTodo: (message: string) => void;
+  removeTodo: (todoId: TodoId) => Promise<void>;
+  addTodo: (message: string) => Promise<void>;
+  completeTodo: (todoId: TodoId) => Promise<void>;
   allTodos: TodoVo[];
 };
