c1e8a76411866d944f085c0cd104601abd0fce46
Gustavo Marques <gugamello2014@yahoo.com>
2024-06-12 19:26:14 -0300

    refactor: more variables

--- src/infra/persistence/firebase/init.ts
+++ src/infra/persistence/firebase/init.ts
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCmh3qIHTaBEANCJrCQpI7hBWQYckNqE5o',
-  authDomain: 'better-focus-51c3c.firebaseapp.com',
-  projectId: 'better-focus-51c3c',
-  storageBucket: 'better-focus-51c3c.appspot.com',
-  messagingSenderId: '819732256353',
-  appId: '1:819732256353:web:0902ae6c3a07f6f70b7c20',
-  measurementId: 'G-00BTTXRC8G',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
