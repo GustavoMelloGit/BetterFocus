3cd8898749da736eac42d83f226684bdde9a6398
Gustavo Marques <gugamello2014@yahoo.com>
2024-06-11 21:04:24 -0300

    style: change default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
+    fontFamily: {
+      'sans': ['Lato', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
