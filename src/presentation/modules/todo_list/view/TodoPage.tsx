12ccbeefa434053a20e3cd807f719fdb75069f09
Gustavo Marques <gugamello2014@yahoo.com>
2024-06-12 22:01:50 -0300

    refactor: move page to modules folder

---
+++ src/presentation/modules/todo_list/view/TodoPage.tsx
@@ -0,0 +1,29 @@
+import { component$, useStore } from "@builder.io/qwik";
+import { useListLoader } from "~/routes";
+import TaskForm from "../components/TaskForm";
+import TaskList from "../components/TaskList/TaskList";
+
+export default component$(() => {
+  const loadedList = useListLoader();
+  const list = useStore(loadedList.value);
+
+  return (
+    <div class="center flex-1 flex-col text-white">
+      <div class="w-full max-w-96 rounded-lg p-4">
+        <div class="mb-4">
+          <h1 class="inline-block bg-gradient-to-r from-pink-600 to-indigo-400 bg-clip-text text-2xl font-bold text-transparent">
+            Todo app
+          </h1>
+        </div>
+
+        <div class="mb-4">
+          <TaskForm currentList={list} />
+        </div>
+
+        <section>
+          <TaskList list={list} />
+        </section>
+      </div>
+    </div>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
 import { makeFetchTasksUseCase } from "~/infra/di/make_fetch_tasks_use_case";
-import TaskForm from "~/presentation/modules/todo_list/components/TaskForm";
-import TaskList from "~/presentation/modules/todo_list/components/TaskList/TaskList";
+import TodoPage from "~/presentation/modules/todo_list/view/TodoPage";
 
 const fetchTasksUseCase = makeFetchTasksUseCase();
 
@@ -11,28 +10,7 @@ export const useListLoader = routeLoader$(async () => {
 });
 
 export default component$(() => {
-  const loadedList = useListLoader();
-  const list = useStore(loadedList.value);
-
-  return (
-    <div class="center flex-1 flex-col text-white">
-      <div class="w-full max-w-96 rounded-lg p-4">
-        <div class="mb-4">
-          <h1 class="inline-block bg-gradient-to-r from-pink-600 to-indigo-400 bg-clip-text text-2xl font-bold text-transparent">
-            Todo app
-          </h1>
-        </div>
-
-        <div class="mb-4">
-          <TaskForm currentList={list} />
-        </div>
-
-        <section>
-          <TaskList list={list} />
-        </section>
-      </div>
-    </div>
-  );
+  return <TodoPage />;
 });
 
 export const head: DocumentHead = {
