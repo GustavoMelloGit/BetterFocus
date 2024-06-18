ab395dd0d11d8dfb37955ba9ab4b3ff7c4129feb
Gustavo Marques <gugamello2014@yahoo.com>
2024-06-18 14:21:24 -0300

    feat: add environment validation

--- src/root.tsx
+++ src/root.tsx
@@ -1,30 +1,35 @@
-import { component$ } from '@builder.io/qwik';
+import { component$ } from "@builder.io/qwik";
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from '@builder.io/qwik-city';
-import { RouterHead } from './presentation/components/router-head/router-head';
+} from "@builder.io/qwik-city";
+import { z } from "zod";
+import { RouterHead } from "./presentation/components/router-head/router-head";
 
-import './global.css';
+import "./global.css";
+
+const envSchema = z.object({
+  API_KEY: z.string(),
+  AUTH_DOMAIN: z.string(),
+  PROJECT_ID: z.string(),
+  STORAGE_BUCKET: z.string(),
+  MESSAGING_SENDER_ID: z.string(),
+  APP_ID: z.string(),
+  MEASUREMENT_ID: z.string(),
+});
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
+  envSchema.parse(process.env);
   return (
     <QwikCityProvider>
       <head>
-        <meta charset='utf-8' />
-        <link rel='manifest' href='/manifest.json' />
+        <meta charset="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
         <RouterHead />
         <ServiceWorkerRegister />
       </head>
-      <body lang='en'>
+      <body lang="en">
         <RouterOutlet />
       </body>
     </QwikCityProvider>
