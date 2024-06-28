f1d0cfcb9a27a7fcb1fed4a49f6f3e0033d37ab8
Gustavo Marques <gugamello2014@yahoo.com>
2024-06-27 22:10:47 -0300

    refactor: rename test

--- src/domain/entities/task_test.spec.ts
+++ src/domain/entities/task_test.spec.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it } from "vitest";
-import { ValidationError } from "../errors/ValidationError";
+import { ValidationError } from "../errors/validation_error";
 import { Task, TaskProps } from "./task";
 
 describe("Task Entity", () => {
